622c3d2f82629531e9399d0c6805bf61ba13c003
Lukraste <49816981+Lukraste@users.noreply.github.com>
2024-03-08 18:19:06 +0100

    Fix console log error

--- 40bierges/src/views/login/login.jsx
+++ 40bierges/src/views/login/login.jsx
@@ -61,7 +61,7 @@ class Login extends React.Component {
         alert("error " + response.status)
       }
     }).catch(error => {
-      console.log(error)
+      console.log(error.response.data)
     });
   }
 
