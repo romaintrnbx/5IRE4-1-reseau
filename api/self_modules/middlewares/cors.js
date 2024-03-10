2cfd6492d24a8e8da1a716f9ee9713b6be9cc152
romaintrnbx <romain.troonbeeckx@gmail.com>
2024-03-10 21:28:38 +0100

    cors fix

--- api/self_modules/middlewares/cors.js
+++ api/self_modules/middlewares/cors.js
@@ -1,12 +1,12 @@
-var whitelist = ['https://react-reseau-kappa.vercel.app', 'https://react-reseau-kappa.vercel.app/index', 'http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001'];
+var whitelist = ['https://react-reseau-kappa.vercel.app','https://react-reseau-kappa.vercel.app/index','http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001']
 
 module.exports = corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
-            let corsOption = { origin: true, methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true };
-            callback(null, corsOption);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
+      let corsOption = {origin: true}
+      callback(null, corsOption)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-};
+  }
+}
