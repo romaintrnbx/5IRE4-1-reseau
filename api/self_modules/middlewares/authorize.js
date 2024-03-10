7c53227c55c1892bd9b488a39b05d354c2e1fa42
MauryTiteux <maury.titeux@gmail.com>
2024-03-10 21:16:34 +0100

    fix acces token oubie

--- api/self_modules/middlewares/authorize.js
+++ api/self_modules/middlewares/authorize.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, callback) => {
-    jwt.verify(req.headers.token, process.env.ACCESS_TOKEN_SECRET, (error, payload) => {
+    jwt.verify(req.headers.token, 'xxxx', (error, payload) => {
         if (error) {
             res.status(500).send(error + '. Please contact the webmaster')
         } else {
