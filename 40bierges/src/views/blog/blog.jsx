324069e7393685743d4aad93c6843f95d0f62f08
romaintrnbx <romain.troonbeeckx@gmail.com>
2024-03-13 18:34:07 +0100

    fixxx

--- 40bierges/src/views/blog/blog.jsx
+++ 40bierges/src/views/blog/blog.jsx
@@ -87,7 +87,7 @@ class Blog extends React.Component {
                     {this.state.messages.map((message, index) => {
                         return (
                             <div key={index}>
-                                <p dangerouslySetInnerHTML={{ __html:message }} />
+                                <p dangerouslySetInnerHTML={{ __html:index+1 +". " +message }} />
                             </div>
                         )
                     })}
